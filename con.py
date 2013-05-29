6eb4936ffc8a80a5a7f18d1fee714025e029e7d6
Rubén Gómez <ruben@irontec.com>
2013-05-29 16:43:51 +0200

    Possible config files
    First approach to config files

---
+++ con.py
@@ -0,0 +1,14 @@
+resolution = dict(
+    x = 1920,
+    y = 1080,
+)
+machines = dict(
+    dict(
+        name = "genesis",
+        pic = "genesis.png",
+    ),
+    dict(
+        name = "snes",
+        pic = "genesis.png"
+    )
+)
